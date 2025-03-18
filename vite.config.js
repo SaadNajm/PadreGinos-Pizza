a9dc41b53de21e2cec291225f13b3941a25b7f2b
Saad <saadnajm.msc@gmail.com>
2025-03-18 08:44:09 +0000

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// replace export
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+      "/public": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [react()],
+});
